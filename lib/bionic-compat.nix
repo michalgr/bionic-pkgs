4a188f1c3d58e00c9258697d06cd4585f175c545
Michał Gregorczyk <michalgr@users.noreply.github.com>
2026-09-10 01:44:22 +0200

    Update bionic-compat.nix

--- lib/bionic-compat.nix
+++ lib/bionic-compat.nix
@@ -141,7 +141,6 @@ in
   # Custom Android 14+ (API 34) Bionic libc & NDK r27 sysroot with built-in shims
   bionic = final.callPackage ../pkgs/libs/bionic { };
 
-
   # Map zlib to Android platform NDK stubs so all packages bind directly to /system/lib64/libz.so
   zlib = final.bionic // {
     dev = final.bionic;
